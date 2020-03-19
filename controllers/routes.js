ddf38666107a2ffbcabcd20f6c118fdf701f6aa5
alexiscatnip <alexiscatnip@gmail.com>
2020-03-19 09:49:07 +0800

    Finalise /routes.

--- controllers/routes.js
+++ controllers/routes.js
@@ -3,31 +3,7 @@
  * GET /map
  * Home page.
  */
-const Client = require("@googlemaps/google-maps-services-js");
-
 exports.getRoute = (req, res) => {
-
-  const client = new Client({});
-
-  client
-    .elevation({
-      params: {
-        locations: [{ lat: 45, lng: -110 }],
-        key: process.env.GOOGLE_MAPS_API_KEY
-      },
-      timeout: 1000 // milliseconds
-    })
-    .then(r => {
-      console.log(r.data.results[0].elevation);
-    })
-    .catch(e => {
-      console.log(e);
-  });
-
-
-
-
-
   res.render('route/route', {
     title: 'Google Maps API'
   });
